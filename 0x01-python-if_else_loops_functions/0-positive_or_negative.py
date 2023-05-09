bc3aa5f742f08ac6fc8d46a47ed57483c9c25673
simonkari <kariithisimon1715@gmail.com>
2023-05-09 20:33:56 +0300

    negative or negative numbers

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,6 +6,6 @@ if number < 0:
     print(f"{number} is negative")
 else:
     if number == 0:
-        print(f"{number} is equal to 0")
+        print(f"{number} is equal to zero")
     else:
         print(f"{number} is positive")
