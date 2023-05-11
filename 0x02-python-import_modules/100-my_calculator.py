49b28e8108e419b73a7757eebaa6c18e2beb5cb9
simonkari <kariithisimon1715@gmail.com>
2023-05-11 22:44:20 +0300

    calculator basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+if len(sys.argv) != 4:
+    print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+    sys.exit(1)
+
+a = int(sys.argv[1])
+b = int(sys.argv[3])
+operator = sys.argv[2]
+
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+print("{} {} {} = {}".format(a, operator, b, result))
+exit()
