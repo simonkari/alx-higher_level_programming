1b81ce4d95f00b0aa20766e5a77d98d51723b8cc
simonkari <kariithisimon1715@gmail.com>
2023-05-11 20:37:34 +0300

    calculator functions and results

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,17 +3,8 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-if __name__ == "__main__":
-
-    def add(a, b):
-        return a + b
-
-    def sub(a, b):
-        return a - b
-
-    def mul(a, b):
-        return a * b
-
-    def div(a, b):
-        return a / b
-    exit()
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
+print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+print("{0} / {1} = {2}".format(a, b, div(a, b)))
+exit()
