25639421c9a7bfd8d0353a7b6d9a2f0bca20f373
simonkari <kariithisimon1715@gmail.com>
2023-05-11 20:33:33 +0300

    calculator functions and results

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+
+if __name__ == "__main__":
+
+    def add(a, b):
+        return a + b
+
+    def sub(a, b):
+        return a - b
+
+    def mul(a, b):
+        return a * b
+
+    def div(a, b):
+        return a / b
+    exit()
