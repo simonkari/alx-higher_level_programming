333756dbed64fa96ea12cb67beab09ee3367493b
simonkari <kariithisimon1715@gmail.com>
2023-05-11 22:57:07 +0300

    calculator basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
 
 import sys
+
 from calculator_1 import add, sub, mul, div
 
+args = sys.argv[1:]
+operators = {'+': add, '-': sub, '*': mul, '/': div}
 
-if len(sys.argv) != :
-    print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
-    sys.exit(1)
-
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-operator = sys.argv[2]
-
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-print("{} {} {} = {}".format(a, operator, b, result))
+if __name__ == "__main__":
+    if len(args) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        if args[1] not in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        else:
+            a = int(args[0])
+            b = int(args[2])
+            operator = args[1]
+            print(f"{a} {operator} {b} = {operators[operator](a, b)}")
+            exit()
