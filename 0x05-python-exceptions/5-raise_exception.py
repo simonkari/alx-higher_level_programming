b8c3d9b92b5a19f0a8fec190ded2a886f66d5fc5
simonkari <kariithisimon1715@gmail.com>
2023-05-29 20:48:48 +0300

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -3,6 +3,11 @@
 def raise_exception():
     raise Exception("This is an exception")
     raise TypeError("Error Occured")
+    raise ValueError("Error Occured")
+    raise NameError("Error Occured")
+    raise SyntaxError("Error Occured")
+    raise IndentationError("Error Occured")
 
 
+raise_exception()
 print()
