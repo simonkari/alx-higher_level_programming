19db72baaf7988a393cf15aa264decac16b43ec2
simonkari <kariithisimon1715@gmail.com>
2023-05-29 19:39:05 +0300

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
-    sk = 0
-    count = 0
-    try:
-        for sk in range(x):
-            print(my_list[sk], end=' ')
-            count += 1
-    except:
-        pass
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            continue
     print()
-    return count
+    return printed
