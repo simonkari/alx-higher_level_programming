45e5aed350f582713b5f66f16e065b0ad9cd925f
simonkari <kariithisimon1715@gmail.com>
2023-05-30 19:36:26 +0300

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    pass
+    def __init__(self, side_length):
+        self.side_length = side_length
+
+    def get_area(self):
+        return self.side_length ** 2
+
+    def get_perimeter(self):
+        return 4 * self.side_length
