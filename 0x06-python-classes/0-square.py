d31ef858375a364222cb36e24565849ba6e52490
simonkari <kariithisimon1715@gmail.com>
2023-05-30 19:40:09 +0300

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+"""Define a class Square"""
 class Square:
-    def __init__(self, side_length):
-        self.side_length = side_length
-
-    def get_area(self):
-        return self.side_length ** 2
-
-    def get_perimeter(self):
-        return 4 * self.side_length
+    """empty class for square."""
+    pass
