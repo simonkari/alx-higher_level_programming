a04c4a8c44d8625db58267aed2b32310e40f9370
simonkari <kariithisimon1715@gmail.com>
2023-05-30 21:18:24 +0300

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,69 +6,57 @@
 
 class Square:
     """
-    class square with attributes:
+    class square that has attributes:
         size
-    some of the attributes are protected from input.
-    instantiation with optional size.
+    some attributes are protected from input.
     """
-
     def __init__(self, size=0):
         """
-        initialization of data structure
+        initialization function for our square clasee
         """
-
         if self.__validate_size(size):
             self.__size = size
 
     @property
     def size(self):
         """
-        fetch size property
+        getter for the size property
         """
-
         return self.__size
 
     @size.setter
     def size(self, value):
         """
-        establish the size property
+        setter for the size property
         """
-
         if self.__validate_size(value):
             self.__size = value
 
     def area(self):
         """
-        calculates area of square
+        calculates the area of the square
         """
-
         return self.__size ** 2
 
     def my_print(self):
         """
-        prints the square '#' characters
+        prints the square using '#' characters
         """
-
         i = 0
         for i in range(0, self.__size):
             j = 0
-
             for j in range(0, self.__size):
                 print("#", end='')
             print()
 
     def __validate_size(self, size):
         """
-        validates the size
+        validates the size, checking for errors
         """
-
         if type(size) != int:
             raise TypeError("size must be an integer")
-
         elif size < 0:
             raise ValueError("size must be >= 0")
-
         else:
             return True
-
         return False
