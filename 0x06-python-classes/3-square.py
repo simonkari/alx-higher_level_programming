68ca6776c9a6f17a462490db9be67102d0c6b834
simonkari <kariithisimon1715@gmail.com>
2023-05-30 20:12:36 +0300

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    some attributes are protected from input.
+    """
+
+    def __init__(self, size=0):
+        """
+        initialization function
+        """
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+            self.__size = size
+
+    def area(self):
+        """
+        return the area of the square
+        """
+
+        return self.__size ** 2
