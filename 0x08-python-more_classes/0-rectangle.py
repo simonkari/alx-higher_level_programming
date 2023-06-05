ff6c70e7caf51cb81a7de68f071d5bee89bb72a6
simonkari <kariithisimon1715@gmail.com>
2023-06-05 17:35:11 +0300

    defining a rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Rectangle class specifically built for
+alx-higher_level_programming Project 0x08 Task 0.
+"""
 
 
 class Rectangle:
-
+    """An empty class as per the task instructions,
+    intended to be further developed in subsequent tasks.
+    """
     pass
