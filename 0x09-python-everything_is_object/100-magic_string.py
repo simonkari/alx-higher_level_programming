47e57b6cd305f7b245bbd32f2a225708f60d0c0f
simonkari <kariithisimon1715@gmail.com>
2023-06-06 20:47:45 +0300

    100-magic_string.py

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def magic_string():
+
+    if hasattr(magic_string, 'calls'):
+
+        magic_string.calls += 1
+    else:
+        magic_string.calls = 1
+    return ', '.join(['BestSchool'] * magic_string.calls)
