2c10b7b1c34600b3a9d2e2a7817dd911254cf05a
simonkari <kariithisimon1715@gmail.com>
2023-06-06 20:53:06 +0300

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def magic_string():
-    return "BestSchool" * n
+def magic_string(n):
+    result = ""
+    for sk in range(1, n + 1):
+        result += "BestSchool" * sk
+    return result
