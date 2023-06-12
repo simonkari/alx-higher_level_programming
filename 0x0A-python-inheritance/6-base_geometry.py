33c9b630791de25a0f10827db25c4310ca8d7368
simonkari <kariithisimon1715@gmail.com>
2023-06-12 22:38:49 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""task 6 is about Geometry improvements"""
+
+
+class BaseGeometry:
+
+    """An empty area() method.
+
+    """
+
+    def area(self):
+
+        """Raises exception to notify user.
+
+        """
+
+        raise Exception('area() is not implemented')
