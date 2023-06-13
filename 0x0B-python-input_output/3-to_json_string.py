8fed88e95bd2c58e07cc396c3aef27fcacc15235
simonkari <kariithisimon1715@gmail.com>
2023-06-13 20:30:41 +0300

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+"""task 5 defines a string-to-JSON function """
+
+
+def to_json_string(my_obj):
+
+    """Return the JSON representation of an object
+
+    Args:
+        my_obj (any): the object to be serialized
+
+    """
+
+    import json
+    return json.dumps(my_obj)
