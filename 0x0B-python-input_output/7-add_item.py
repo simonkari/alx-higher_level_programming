17f43a2d5fe2d93b7e555e8c17fd202b75986ee4
simonkari <kariithisimon1715@gmail.com>
2023-06-13 20:57:14 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 
 
-"""
-task 9 writes an Object to a text file,
-using a JSON representation
-"""
+"""task 9 writes an Object to a text file,
+using a JSON representation"""
 
 
+import sys
 import json
 
 
-def save_to_json_file(my_obj, filename):
+def save_to_json_file(obj, filename):
+    with open(filename, 'w') as file:
+        json.dump(obj, file)
 
-    """
-    writes an Object to a text file,
-    using a JSON representation
-    """
 
-    with open(filename, 'w') as f:
+def load_from_json_file(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
 
-        json.dump(my_obj, f)
+
+if __name__ == '__main__':
+    argv_edit = sys.argv[1:]
+
+    try:
+        content_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        content_list = []
+    finally:
+        content_list.extend(argv_edit)
+        save_to_json_file(content_list, "add_item.json")
