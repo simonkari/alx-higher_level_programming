e69c5d3f502dbeb04d942c822c4269938ac09bf7
simonkari <kariithisimon1715@gmail.com>
2023-06-19 22:04:39 +0300

    models/

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,6 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        # attribute assigment here engages setters defined below
         self.width = width
         self.height = height
         self.x = x
