c38b3f428d7fc3ecc24e92a6dc0644182f8e2b90
simonkari <kariithisimon1715@gmail.com>
2023-07-18 22:51:07 +0300

    15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,11 +1,7 @@
 -- The script used to list the number of records with the same score in the table.
-
 -- second_table of the database hbtn_0c_0.
-
 -- result to display the score.
-
 -- number of records for this score with the label number.
-
 -- list should be sorted by the number of records (descending).
 
 from second_table
