ca77c926bd219505593e28a07a92754331293063
simonkari <kariithisimon1715@gmail.com>
2023-07-18 22:46:15 +0300

    12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,7 @@
 -- The script that updates score of Bob to 10 in the table second_table.
+
 -- you are not allowed to use Bob’s id value, only the name field.
+
 UPDATE second_table
 SET score = 10
 WHERE `name` = 'Bob';
