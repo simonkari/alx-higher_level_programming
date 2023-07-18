d592c83d66236402ec3e95734ac7f8bc0033fa32
simonkari <kariithisimon1715@gmail.com>
2023-07-18 22:35:47 +0300

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
--- The Script that list tables
--- The database name will be passed as argument of mysql command
+-- The Script used to list tables.
+-- The database name will pass as argument of mysql command.
+
 SHOW TABLES;
