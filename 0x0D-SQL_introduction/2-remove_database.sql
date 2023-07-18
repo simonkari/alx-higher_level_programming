42bce2b51828e44f31b48c6fd11ac4f7b1852778
simonkari <kariithisimon1715@gmail.com>
2023-07-18 22:33:41 +0300

    2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
--- script that deletes a database
--- If database doesn’t exist, the script should not fail
+-- The script that deletes a database.
+-- If database doesn’t exist, the script should not fail.
+
 DROP DATABASE IF EXISTS hbtn_0c_0;
