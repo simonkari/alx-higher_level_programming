6349dfc1acae742392e2103250533c4fa8b86df9
simonkari <kariithisimon1715@gmail.com>
2023-07-18 22:37:31 +0300

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
--- A script that creates a table called first_table in the current database.
--- id INT
--- name VARCHAR(256)
+-- The script that creates a table called first_table in the current database.
+-- id INT.
+-- name VARCHAR(256).
+
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
