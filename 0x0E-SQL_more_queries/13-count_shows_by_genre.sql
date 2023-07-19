45fff19b15fe469293436d35c065a04154f8454c
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:07:47 +0300

    13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
--- Script that lists genres and number of shows
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows -- Query to join tables
-FROM tv_show_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+-- A script used to lists genres and number of shows.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows DESC, tv_genres.id ASC;
+ORDER BY number_of_shows DESC;
