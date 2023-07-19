63c6fb1c97e25659af3bb768deaed37314591979
simonkari <kariithisimon1715@gmail.com>
2023-07-19 19:57:58 +0300

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,6 @@
--- Script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name -- Query to join cities and states
-FROM cities
-JOIN states ON cities.state_id = states.id;
+-- A script used to lists all cities contained in the database.
+-- 9-cities_by_state_join.sql.
+
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
