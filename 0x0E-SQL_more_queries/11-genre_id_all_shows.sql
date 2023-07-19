900efc5a0107a6f10d70fa1b661b727fb6b118a8
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:03:21 +0300

    11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
--- Script that lists all cities contained in the database
-SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- A script used to lists all cities contained in the database.
+-- The database name will be passed as an argument of the mysql command.
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
