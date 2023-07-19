d6ce8a7e4c888fdd4a92db76193a1df78e2e5b96
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:10:11 +0300

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,8 @@
--- Script that lists genres of Dexter show
-SELECT tv_genres.name -- Query to join tables
-FROM tv_genres
-     JOIN tv_show_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
-     JOIN tv_shows
-     	  ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;
+-- A script used to lists genres of Dexter show.
+-- The database name will be passed as an argument of the mysql command
+
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name;
