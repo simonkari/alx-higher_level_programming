d4ac42253cdbb221b06dd1f1f9524e97ca5b6ebd
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:16:20 +0300

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- A script used to lists all shows and all genres.
---sort in ascending order by the show title and genre
 
 SELECT title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
