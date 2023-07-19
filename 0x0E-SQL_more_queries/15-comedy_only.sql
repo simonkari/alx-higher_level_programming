a9b1e670b8936d4b978fe6be00ef9436bfe8a688
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:12:24 +0300

    15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,8 @@
--- Script that lists genres of Dexter show
-SELECT tv_shows.title -- Query to join tables
-FROM tv_shows
-     JOIN tv_show_genres
-     	  ON tv_show_genres.show_id = tv_shows.id
-     JOIN tv_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+-- A script used lists genres of Dexter show.
+-- Results must be sorted in ascending order by the show title.
+
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY title;
