f92026a7f7bef73a67109874daa014991c123aee
simonkari <kariithisimon1715@gmail.com>
2023-07-19 19:07:16 +0300

    Adding privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- The script that lists all privileges of MySQL users.
--- Query used to list all privileges of the MySQL users.
-
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
