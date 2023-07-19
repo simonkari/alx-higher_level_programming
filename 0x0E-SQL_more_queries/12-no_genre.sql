291f0957dd4d035919f1ddd477e1ac7139df2226
simonkari <kariithisimon1715@gmail.com>
2023-07-19 20:06:08 +0300

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
--- Script that lists all cities contained in the database
-SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id is NULL
-ORDER BY tv_shows.title;
+-- A script used to lists all cities contained in the database.
+-- 12-no_genre.sql
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
