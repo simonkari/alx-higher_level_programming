027904542e5c7d4ac8c5d508a82d3335f8a9faef
simonkari <kariithisimon1715@gmail.com>
2023-07-19 19:12:38 +0300

    Adding privileges of the MySQL users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
--- 0x0E. SQL - More queries, task 0.
---My privileges!
--- Lists all priviledges for users
---`user_0d_1` and `user_0d_2`
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- 0x0E. SQL - More queries, task 0. My privileges!
+-- Lists all priviledges for users `user_0d_1` and `user_0d_2`
+SELECT * FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2');
