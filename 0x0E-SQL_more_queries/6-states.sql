5c4f67d0bbb9eb7d073857c09873a6a90756eb20
simonkari <kariithisimon1715@gmail.com>
2023-07-19 19:48:54 +0300

    6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
--- Script that creates a database and a table
-CREATE DATABASE -- Query to create database hbtn_0d_usa
-IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states ( -- Query to create states table with 2 constraints
-       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+-- A script used to create a database and a table.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
        name VARCHAR(256) NOT NULL);
