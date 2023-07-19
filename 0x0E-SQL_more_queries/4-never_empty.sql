09ac74de7d54ee0a2c052776cd60aa3d5ca57c49
simonkari <kariithisimon1715@gmail.com>
2023-07-19 19:35:49 +0300

    4-never_empty.sql

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
--- Script that creates a table
--- Query to create the table 'id_not_null' in MySQL server
-CREATE TABLE IF NOT EXISTS id_not_null (
-       id INT DEFAULT 1,
-       name VARCHAR(256));
+-- A script that creates a table.
+-- The query used to create the table 'id_not_null' in MySQL server.
+--database name will be passed as an argument of the mysql command.
+
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
