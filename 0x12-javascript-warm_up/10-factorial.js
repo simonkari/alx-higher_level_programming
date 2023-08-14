c401f43d0f3eb40f21ae9333c5aef7fdc56a0f47
simonkari <kariithisimon1715@gmail.com>
2023-08-14 14:41:19 +0300

    factorials

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const num = parseInt(process.argv[2]);
+function factorial(n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
 
-const factorial = n => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-};
-
+const num = Number(process.argv[2]);
 console.log(factorial(num));
