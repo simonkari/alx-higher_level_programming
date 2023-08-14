e25b24f5909a1f0c0c95dc95da92378f6a9c4e6f
simonkari <kariithisimon1715@gmail.com>
2023-08-14 14:17:04 +0300

    a tow argument script

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-
-const [, , arg1, arg2] = process.argv;
-if (arg1 && arg2) {
-  console.log(`${arg1} is ${arg2}`);
-} else {
-  console.log('Insufficient arguments');
-}
+const argv = require('process').argv;
+console.log(argv[2] + ' is ' + argv[3]);
