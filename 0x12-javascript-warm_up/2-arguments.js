1ed02b91b521957ef146e98d3afdaa4cf9e3ec38
simonkari <kariithisimon1715@gmail.com>
2023-08-14 14:03:52 +0300

    a script based on argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,8 @@ const argv = require('process').argv;
 
 if (argv.length < 3) {
   console.log('No argument');
-
 } else if (argv.length === 3) {
   console.log('Argument found');
-
 } else {
   console.log('Arguments found');
 }
