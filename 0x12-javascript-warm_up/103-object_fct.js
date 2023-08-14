fe20078e73becd76be527fc9a164c5e06418fc8d
simonkari <kariithisimon1715@gmail.com>
2023-08-14 21:11:57 +0300

    103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function (x, theFunction) {
-  if (x <= 0) return;  // No need to execute the loop if x is not positive
-
-  const batchSize = 10;  // Adjust batch size as needed
-  const fullBatches = Math.floor(x / batchSize);
-  const remainingCalls = x % batchSize;
-
-  for (let i = 0; i < fullBatches; i++) {
-    for (let j = 0; j < batchSize; j++) {
-      theFunction();
-    }
-  }
-
-  for (let i = 0; i < remainingCalls; i++) {
-    theFunction();
-  }
+const myObject = {
+  type: 'object',
+  value: 12
 };
+console.log(myObject);
+myObject.incr = () => myObject.value++;
+myObject.incr();
+
+console.log(myObject);
+myObject.incr();
+
+console.log(myObject);
+myObject.incr();
+
+console.log(myObject);
