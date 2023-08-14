9c0b3e0f497bdc87d41dd52c8b0a4d712ab2b9b7
simonkari <kariithisimon1715@gmail.com>
2023-08-14 14:47:03 +0300

    integer search script

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+    console.log(0);
+  } else {
+    const args = process.argv.slice(2).map(Number);
+    const sortedArgs = args.sort((a, b) => a - b);
+    console.log(sortedArgs[sortedArgs.length - 2]);
+  }
+  
