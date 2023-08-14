64eeb1b0af2bbb68a4d205db84fbe6f4c6df6c3e
simonkari <kariithisimon1715@gmail.com>
2023-08-14 21:03:35 +0300

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (x, theFunction) {
-  if (typeof x !== 'number' || !Number.isInteger(x) || x <= 0) {
-    throw new Error('Invalid input: x must be a positive integer.');
-  }
-
-  if (typeof theFunction !== 'function') {
-    throw new Error('Invalid input: theFunction must be a function.');
-  }
-
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
+  
+  for (let i = 0; i < x; i++) theFunction();
 };
