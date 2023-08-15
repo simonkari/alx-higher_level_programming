8aa38659f05e5011d7225abe5ce8d54a1f5b87b6
simonkari <kariithisimon1715@gmail.com>
2023-08-15 18:13:53 +0300

    2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,8 +4,7 @@ class Rectangle {
   constructor (w, h) {
     if ((w = parseInt(w)) > 0 &&
 
-        (h = parseInt(h)) > 0) 
-        {
+        (h = parseInt(h)) > 0){
       this.width = w;
       this.height = h;
     }
