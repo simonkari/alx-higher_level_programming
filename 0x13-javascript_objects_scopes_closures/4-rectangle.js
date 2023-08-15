5a6dfc1a98a7056c73883143d10b8d44bcaefc0c
simonkari <kariithisimon1715@gmail.com>
2023-08-15 18:27:06 +0300

    4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -10,21 +10,18 @@ class Rectangle {
   }
 
   print () {
-
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-
     this.width += this.height;
     this.height = this.width - this.height;
     this.width -= this.height;
   }
 
   double () {
-
     this.width *= 2;
     this.height *= 2;
   }
