56495a9df64015a5a9c308681d49632ff3cd579c
simonkari <kariithisimon1715@gmail.com>
2023-08-15 19:10:24 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,7 +2,6 @@
 
 // imports a dictionary of occurrences by user id and computes a dictionary of
 // user ids by occurrence
-
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const key in dict) {
@@ -12,4 +11,5 @@ for (const key in dict) {
     newDict[dict[key]].push(key);
   }
 }
+
 console.log(newDict);
