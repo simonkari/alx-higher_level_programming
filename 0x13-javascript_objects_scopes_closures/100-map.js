04d89844b9d23b3edf794a73ac704d213a8bed97
simonkari <kariithisimon1715@gmail.com>
2023-08-15 19:02:09 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 
 // imports an array and computes a new array
 
-const property = require('./100-data.js').property;
+const list = require('./100-data.js').list;
 console.log(list);
 console.log(list.map((x, xI) => x * xI));
