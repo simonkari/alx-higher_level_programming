3772d410bcd091b7d9a488d08001d9fda11f8e9f
simonkari <kariithisimon1715@gmail.com>
2023-08-15 18:16:58 +0300

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    w = parseInt(w);
+    h = parseInt(h);
+    
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
