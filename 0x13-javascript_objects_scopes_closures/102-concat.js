2e4f2a44a917d4dc22c0b72c94fd8fed79cb9e8e
simonkari <kariithisimon1715@gmail.com>
2023-08-15 19:13:41 +0300

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
+
 // script that concats 2 files
+
 const fs = require('fs');
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
 
-async function concatenateFiles() {
-  try {
-    const file1Data = await fs.promises.readFile(process.argv[2]);
-    const file2Data = await fs.promises.readFile(process.argv[3]);
-    
-    const concatenatedData = file1Data + file2Data;
-    
-    await fs.promises.writeFile(process.argv[4], concatenatedData);
-    
-    console.log('Files concatenated successfully.');
-  } catch (err) {
-    console.error('An error occurred:', err);
-  }
-}
-
-concatenateFiles();
+fs.writeFileSync(process.argv[4], src1 + src2);
