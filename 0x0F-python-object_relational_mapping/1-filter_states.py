8bda3541a0c6638cd0e2fe6de187da1c01c47600
simonkari <kariithisimon1715@gmail.com>
2023-08-21 12:56:14 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,19 +4,18 @@ list all states starting with n'
 """
 
 # import module
-from sys import argv
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        charset="utf8",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
-     # Create a cursor to execute SQL queries
+
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
