c3b54daad7b08073222b0f8ec1bd8cf926db0d6e
simonkari <kariithisimon1715@gmail.com>
2023-08-21 13:47:12 +0300

    relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""Represents a state in the database."""
+""" Declare ORM entity."""
 
-
+import sqlalchemy
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
 class State(Base):
-    """
-Describes a state entity within a MySQL database.
-
-Attributes:
-    __tablename__ (str): The name of the table storing state records.
-    id (sqlalchemy.Integer): The unique identifier of the state.
-    name (sqlalchemy.String): The name of the state.
-    cities (sqlalchemy.orm.relationship): Relationship with associated cities.
-"""
-
+    """ Object-Relational Mapping (ORM) representation of a state. """
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        unique=True,
+        nullable=False
+    )
     name = Column(String(128), nullable=False)
     cities = relationship(
         "City",
