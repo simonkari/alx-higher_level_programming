c9dc9115cbec90960507640a6098621b1b1220b8
simonkari <kariithisimon1715@gmail.com>
2023-08-21 11:19:47 +0300

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 """
-Lists all State from a database.
+Lists all State objects.
 """
 
-# import modules
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+def main():
+    try:
+        engine = create_engine(
+            f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+            pool_pre_ping=True
+        )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+
+        with Session() as session:
+            states = session.query(State).order_by(State.id).all()
+            for state in states:
+                print(f"{state.id}: {state.name}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
