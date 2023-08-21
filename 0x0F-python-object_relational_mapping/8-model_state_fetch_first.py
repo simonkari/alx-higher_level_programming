b3a0aab6c5437cceb0615940191f69d033bc3868
simonkari <kariithisimon1715@gmail.com>
2023-08-21 11:30:45 +0300

    8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""states, fetch first"""
+"""
+Lists the first State object from a database.
+"""
 
+# import module
+import sqlalchemy
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-def main():
-    try:
-        username, password, database = argv[1], argv[2], argv[3]
-
-        engine = create_engine(
-            f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
-            pool_pre_ping=True
-        )
-
-        Base.metadata.create_all(engine)
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        state = session.query(State).order_by(State.id).first()
-        if state:
-            print(f"{state.id}: {state.name}")
-        else:
-            print("Nothing")
-
-        session.close()
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
 if __name__ == "__main__":
-    main()
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # Create the tables in the database based on the defined models
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        # print information about the state
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    # close the session
+    session.close()
