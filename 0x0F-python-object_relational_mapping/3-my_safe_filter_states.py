550e993d2f2ffae9e78ea1123d73c28028f483e9
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:47:20 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
 
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
+
     for row in query_rows:
         if row[1].startswith("N"):
             print(row)
