e2911247dbdb56576b6c16803a43a598072cc5db
simonkari <kariithisimon1715@gmail.com>
2023-08-21 13:16:19 +0300

    relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,9 +4,12 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create a declarative base class for SQLAlchemy models
+Base = declarative_base()
 
 class City(Base):
-    """Represents a city for a MySQL database.
+    """
+    Represents a city for a MySQL database.
 
     Attributes:
         id (sqlalchemy.Column): The city's id.
