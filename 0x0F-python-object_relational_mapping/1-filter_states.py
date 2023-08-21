f0c016e54f7e5e71c41b2f1abf7e406c55234ef5
simonkari <kariithisimon1715@gmail.com>
2023-08-21 12:48:51 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,8 +4,8 @@ list all states starting with n'
 """
 
 # import module
+from sys import argv
 import MySQLdb
-import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
