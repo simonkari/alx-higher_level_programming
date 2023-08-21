7291ad52113ff59683a4984b6310093dad49b71f
simonkari <kariithisimon1715@gmail.com>
2023-08-21 12:39:49 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""list all states starting with n"""
+"""
+list all states starting with 'n'
+"""
 
 # import module
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        if row[1].startswith("N"):
-            print(row)
-    # close the connection
-    cur.close()
-    conn.close()
+    import MySQLdb
+    from sys import argv
+
+    # Database connection parameters
+    host = "localhost"
+    port = 3306
+    charset = "utf8"
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    query = "SELECT id, name FROM states ORDER BY id ASC"
+
+    with MySQLdb.connect(host=host, port=port, charset=charset, user=user, passwd=password, db=database) as conn:
+        with conn.cursor() as cur:
+            cur.execute(query)
+
+            # Fetch and process data in smaller batches
+            batch_size = 100
+            while True:
+                batch_rows = cur.fetchmany(batch_size)
+                if not batch_rows:
+                    break
+                for row in batch_rows:
+                    print(row)
