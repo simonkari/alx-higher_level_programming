ee45751d3abdaa85595d605acee3709ba2c127a5
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:52:49 +0300

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script used to select cities by states from the
+database hbtn_0e_4_usa.
+"""
+
+# import modules
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT cities.id, cities.name, states.name FROM cities
+        LEFT JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC;
+        """)
+    
+    # fetch all rows of the query
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
