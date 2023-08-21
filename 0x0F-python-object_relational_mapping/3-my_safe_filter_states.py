19a49a14acca59cc9fc07fe18659f18fa45076bc
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:43:38 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-"""list all states with n"""
+"""
+This script retrieves and displays rows from the 'states'
+table of the 'hbtn_0e_0_usa' database where the 'name'
+column matches the provided argument.
+"""
 
+# import module
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+
+    """
+    Main script to retrieve and display rows from the 'states' table.
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
@@ -13,5 +22,7 @@ if __name__ == "__main__":
     for row in query_rows:
         if row[1].startswith("N"):
             print(row)
+    
+    # close the connection
     cur.close()
     conn.close()
