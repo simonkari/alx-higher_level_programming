5f4b363498ba956c6952978ed55184f6b0a3861b
simonkari <kariithisimon1715@gmail.com>
2023-08-21 12:03:08 +0300

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,27 +10,19 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-def main():
-    if len(sys.argv) != 4:
-        print("Usage: python script_name.py username password dbname")
-        return
-
-    connection_string = (
-        f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}"
-    )
-
-    try:
-        engine = create_engine(connection_string, pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-
-        Session = sessionmaker(bind=engine)
-        with Session() as session:
-            state = session.query(State).filter_by(id=2).first()
-            if state:
-                state.name = "New Mexico"
-                session.commit()
-    except Exception as e:
-        print("An error occurred:", e)
-
 if __name__ == "__main__":
-    main()
+    engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.id == 2).all()
+    if states:
+        # Update the name of the State
+        states[0].name = "New Mexico"
+    session.commit()
+    # close session
+    session.close()
+    
