bbe0425c34c49979be65d02e8a84a10ff6068fbc
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:34:36 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-""" This script retrieves and prints states
-    whose names start with a capital N from a MySQL database.
-"""
+""" print states starting with capital N"""
 
+#import module
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # Establish a connection to the MySQL database using command-line arguments.
+    
+    # database connection
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-
-    # Define a SQL query to select states with names that match the provided argument.
-    query = """ SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"""
+    query = """ SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY
+        states.id ASC"""
     query = query.format(argv[4])
-    
-    # Execute the query and fetch all the rows that match the criteria.
     cur.execute(query)
     query_rows = cur.fetchall()
-
-    # Iterate through the fetched rows and print each row.
     for row in query_rows:
         print(row)
-
-    # Close the cursor and the database connection.
+    
+    # close database connection
     cur.close()
     conn.close()
