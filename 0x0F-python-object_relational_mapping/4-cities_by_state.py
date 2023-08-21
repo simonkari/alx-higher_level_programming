1188f59f0038e2ab625aabda5867c0226afb3779
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:53:31 +0300

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,6 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-
     cur.execute(
         """
         SELECT cities.id, cities.name, states.name FROM cities
