017b9fdfa222f909c592abaf0a247ea73d1bb09b
simonkari <kariithisimon1715@gmail.com>
2023-08-21 13:54:49 +0300

    relationship_city.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
-""" Provide a definition for a city entity."""
+"""
+This script defines SQLAlchemy model for representing
+cities and their states.
+"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
+import sqlalchemy
+
 
 class City(Base):
-    """Class representing a city."""
-    __tablename__ = "cities"  # This sets the name of the table in the database.
-    
+    """city class"""
+    __tablename__ = "cities"
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
-    # The 'id' column serves as the primary key for the 'cities' table.
-    
     name = Column(String(128), nullable=False)
-    # The 'name' column stores the name of the city, limited to 128 characters.
-    
+    """
+    The name of the city.
+
+    Type: str
+    """
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    # The 'state_id' column is a foreign key that references the 'id' column in the 'states' table.
-    # This establishes a relationship between cities and states.
