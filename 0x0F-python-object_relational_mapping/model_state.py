e619b0afbc30eeb83f5e88c8b5cd5149caf86699
simonkari <kariithisimon1715@gmail.com>
2023-08-21 11:08:23 +0300

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script defines a SQLAlchemy model for states.
+"""
+
+# import models from sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ORM class for states."""
+    __tablename__ = "states"
+    
+    id = Column(Integer, primary_key=True, nullable=False,
+                autoincrement=True, unique=True, doc="Primary key of the state.")
+    name = Column(String(128), nullable=False, doc="Name of the state.")
+    
+    # If there are relationships to other tables, define them here
+    # Example: cities = relationship("City", back_populates="state")
+    
+    def __repr__(self):
+        return f"<State(id={self.id}, name={self.name})>"
