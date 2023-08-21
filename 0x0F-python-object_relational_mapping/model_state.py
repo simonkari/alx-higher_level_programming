c844affcb926d891a7c1257347700d495fd655b4
simonkari <kariithisimon1715@gmail.com>
2023-08-21 11:17:52 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-This script defines a SQLAlchemy model for states.
-"""
+""" State ORM object"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 class State(Base):
-    """ORM class for states."""
+    """
+    ORM class for representing states.
+    """
+
     __tablename__ = "states"
-    
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True, index=True)  # Add an index for efficiency
+
+    id = Column(Integer, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
-    
-    # If there are relationships to other tables, define them here
-    # Example: cities = relationship("City", back_populates="state", lazy="joinedload")
-    
+
+    def __init__(self, name):
+        self.name = name
+
     def __repr__(self):
         return f"<State(id={self.id}, name={self.name})>"
-
-# You can add your main code here, which instantiates the session and queries the data.
-# Implement batch processing and connection pooling if needed.
