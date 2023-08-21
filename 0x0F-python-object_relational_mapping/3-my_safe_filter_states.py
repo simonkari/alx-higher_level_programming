acaeecf6e9f6bc0b5d953fd745b9d29dcb6998ac
simonkari <kariithisimon1715@gmail.com>
2023-08-21 10:45:49 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,6 +10,10 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+
+    """
+    Main script to retrieve and display rows from the 'states' table.
+    """
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
@@ -18,7 +22,5 @@ if __name__ == "__main__":
     for row in query_rows:
         if row[1].startswith("N"):
             print(row)
-    
-    # close the connection
     cur.close()
     conn.close()
