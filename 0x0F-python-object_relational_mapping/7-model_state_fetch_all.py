d9798c4604dcbe04403dbe4f0107530ccb858a1c
simonkari <kariithisimon1715@gmail.com>
2023-08-24 11:47:57 +0300

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,9 +4,13 @@
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
+import sqlalchemy
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
+    """
+    Display all State instances stored in a database.
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
