38f8350f5a63cb91631fd6918b9dddbb204a9830
simonkari <kariithisimon1715@gmail.com>
2023-08-24 12:06:51 +0300

    12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,44 +1,29 @@
 #!/usr/bin/python3
 """
-Changes the name of the State object with id=2 to 'New Mexico'
-in the database.
+Changes the name of the State object with id=2 to 'New Mexico'.
 """
 
-# Import the necessary modules from the SQLAlchemy library
+# Import modules from the SQLAlchemy
 import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    """
-    Change the name of the State object with id=2 to 'New Mexico'
-    in the database.
-    """
-    # Create a database connection using SQLAlchemy's create_engine function
-    eng = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+    engine = create_engine(
+                "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
     )
+    Base.metadata.create_all(engine)
 
-    # Create the tables in the database based on the defined models
-    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=engine)
 
-    # Create a session to interact with the database
-    Session = sessionmaker(bind=eng)
     session = Session()
 
-    # Query the State object with id=2
-    state = session.query(State).filter_by(id=2).first()
-
-    # Check if the State object was found
-    if state:
-        # Update the name of the State object to 'New Mexico'
-        state.name = "New Mexico"
-        session.commit()
-        print("State with id=2 has been updated to 'New Mexico'.")
-    else:
-        print("State with id=2 not found.")
-
-    # Close the session when done
+    states = session.query(State).filter(State.id == 2).all()
+    if states:
+        # Update the name of the State
+        states[0].name = "New Mexico"
+    session.commit()
+    # close session
     session.close()
