f3cd491cecc647ff979f038c137cd1072db1d3ea
simonkari <kariithisimon1715@gmail.com>
2023-08-24 11:23:02 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,8 +3,8 @@
 instance of Base using declarative_base(). """
 
 import sqlalchemy
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
