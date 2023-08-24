c478b2dc3c2a98c45c8c522ae962d1bb05ca3b0f
simonkari <kariithisimon1715@gmail.com>
2023-08-24 11:05:39 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
+'''
 A program that chooses and exhibits all the states from the hbtn_0e_0_usa database.
-"""
+'''
 
-import sys
+# Import modules
 import MySQLdb
+import sys
 
-def main():
-    """
-    Primary function for fetching and showing all states from the database.
-    """
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
+if __name__ == "__main__":
+    '''
+    Primary script for fetching and displaying all states from the database.
+    '''
     # Create a database connection using MySQLdb
     db = MySQLdb.connect(
         host="localhost",
@@ -23,23 +20,15 @@ def main():
         db=sys.argv[3]
     )
 
-    try:
-        # Generate a cursor for executing SQL queries
-        with db.cursor() as cur:
-            # Run an SQL query to fetch all states from the "states" table
-            cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-            # Retrieve all rows from the query output
-            rows = cur.fetchall()
-
-            # Loop through the rows and display each row
-            for row in rows:
-                print(row)
-    except MySQLdb.Error as e:
-        print("Error: {}".format(e))
-    finally:
-        # Terminate the database connection
-        db.close()
-
-if __name__ == "__main__":
-    main()
+    # Generate a cursor for executing SQL queries
+    cur = db.cursor()
+    # Run an SQL query to fetch all states from the "states" table
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Retrieve all rows from the query output
+    rows = cur.fetchall()
+    # Loop through the rows and display each row
+    for row in rows:
+        print(row)
+    # Terminate the database connection
+    cur.close()
+    db.close()
