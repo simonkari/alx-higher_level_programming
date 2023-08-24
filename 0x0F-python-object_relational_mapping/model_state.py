f358f3b1d8d643c47bba4e41ed47e2b1fbead52d
simonkari <kariithisimon1715@gmail.com>
2023-08-24 11:33:08 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,10 +13,20 @@ Base = declarative_base()
 
 
 class State(Base):
-    """ State Class inherit from Base declarative_base()
-    links to the MySQL table states
-    Attr:
-        id, name
+    """ 
+    This class serves as an SQLAlchemy model, outlining
+    the structure and characteristics of a state.
+
+    Attributes:
+        __tablename__ (str): The name of the database table
+            where state records are stored.
+
+        id (int): The primary key for state records.
+        name (str): The name of the state.
+
+    Note:
+        The class definition establishes a mapping of
+        this model to the "states" table in the database.
     """
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True, primary_key=True)
