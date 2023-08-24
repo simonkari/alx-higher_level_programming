7714b8b133452fc206fc6d1fc14e99716250093d
simonkari <kariithisimon1715@gmail.com>
2023-08-24 11:30:09 +0300

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""contains the class definition of a State and an instance
-Base = declarative_base() """
+"""
+This script establishes an SQLAlchemy model to represent states.
+"""
 
 # Import modules
 import sqlalchemy
