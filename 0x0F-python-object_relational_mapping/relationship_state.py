ef5e47358fa9cd7db29821b927056b81864647d4
simonkari <kariithisimon1715@gmail.com>
2023-08-24 12:24:04 +0300

    relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
 """
-Defines a SQLAlchemy model for representing
-states and their cities.
+Contains the class definition of a State and an
+instance of Base using declarative_base().
+Base = declarative_base()
 """
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+# Import modules
 import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ ORM class for state """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    """ State class inheriting from declarative_base() of Base.
+    Represents a connection to the MySQL table named 'states'.
+    Attr:
+        id, name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    """
+    The primary key of the state record.
+
+    Type: int
+    """
     name = Column(String(128), nullable=False)
 
-    cities = relationship(
-        "City",
-        cascade="all, delete-orphan",
-        backref=backref("state", cascade="all"),
-        single_parent=True)
+    cities = relationship("City", cascade='delete', backref="state")
