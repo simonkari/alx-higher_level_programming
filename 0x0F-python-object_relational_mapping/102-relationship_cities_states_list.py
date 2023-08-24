89d55f8f7c812c8c30a211a4021b6c376718d666
simonkari <kariithisimon1715@gmail.com>
2023-08-24 12:45:03 +0300

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
+"""Script that lists all objects from a database
 """
-Lists all City objects.
-"""
-
+import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import (create_engine)
-import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from relationship_state import Base, State
+from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    """
-    Retrieve and display information about City objects.
-    """
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost:3306/{}'
-                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    #create session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    cities = session.query(City).order_by(City.id).all()
 
-    #Display the information about cities
-    for city in cities:
+    query = session.query(City).order_by(City.id)
+    for city in query:
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
-    session.commit()
-    # close session
+
     session.close()
+    
