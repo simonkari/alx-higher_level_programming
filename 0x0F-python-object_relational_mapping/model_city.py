6e6d2fab4d2d7f0f9b3995f83662ebaaf16aea40
simonkari <kariithisimon1715@gmail.com>
2023-08-24 12:14:23 +0300

    model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
 """
- SQLAlchemy model for representing
-cities and their associated states.
+Contains the class definition of a State and an
+instance of Base using declarative_base().
+Base = declarative_base()
 """
 
-# Import necessary modules
+# Import modules
 import sqlalchemy
+from sqlalchemy import Column, Integer, String
+from model_state import Base
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
 
-# Create a base class for declarative models
-Base = declarative_base()
 
-# Define the City class, inheriting from the Base class
 class City(Base):
-    """city class"""
-    __tablename__ = "cities"
-    
-    # Define attributes/columns of the City class
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    """ City class inheriting from declarative_base() of Base.
+    Represents a connection to the MySQL table named 'cities'.
+    Attr:
+        id, name
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
+    """
+    The foreign key referencing the associated state's ID.
 
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    Type: int
+    """
