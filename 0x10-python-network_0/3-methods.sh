73891f2d8baf671cd1000974b1a8d7bdbb544b02
simonkari <kariithisimon1715@gmail.com>
2023-08-31 13:49:14 +0300

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Show the complete list of HTTP methods that the server for a provided URL is willing to accept.
-curl -s -I -X OPTIONS "$1"
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
