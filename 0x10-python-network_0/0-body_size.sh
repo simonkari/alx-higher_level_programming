850aa4509b1778dd8e16b5aaaa2463b921bccded
simonkari <kariithisimon1715@gmail.com>
2023-08-31 13:39:59 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
-# This script takes in a URL, sends a request to that URL
-
+# The script takes in a URL, then sends a request to that URL
 curl -sI "$1" | grep Content-Length | cut -d " " -f2
