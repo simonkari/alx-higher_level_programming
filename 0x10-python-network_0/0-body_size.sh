6f6a87c8c21c4c732f282a568b4cdfd67204a01a
simonkari <kariithisimon1715@gmail.com>
2023-08-31 13:38:55 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
+
 # This script takes in a URL, sends a request to that URL
 
-# Check if a URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-content_length=$(curl -sI "$1" | awk -F': ' '/Content-Length/{print $2; exit}')
-
-if [ -n "$content_length" ]; then
-    echo "Content-Length: $content_length"
-else
-    echo "Content-Length header not found."
-fi
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
