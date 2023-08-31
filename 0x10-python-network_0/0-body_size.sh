a38ba6a4e1daf6c2ea73ae9ad9b0c67064bd7cb7
simonkari <kariithisimon1715@gmail.com>
2023-08-31 13:28:11 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # Obtain the size in bytes of the HTTP response header for a provided URL.
 
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+# Check if a URL is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a request to the provided URL and store the response in a variable
+response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
+
+# Display the size of the response body in bytes
+echo "Size of the response body: $response bytes"
