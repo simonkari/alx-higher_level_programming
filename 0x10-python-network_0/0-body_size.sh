6fc40d12e716ef5a984114f6257a0c641a89a031
simonkari <kariithisimon1715@gmail.com>
2023-08-31 13:35:15 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# This script accepts a URL input, initiates a 
-# request to the provided URL
+# This script accepts a URL input,
+# initiates a request to the provided URL
+
 curl -sI "$1" | grep Content-Length | cut -d " " -f2
