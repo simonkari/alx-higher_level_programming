51e818721c911310dc35c83db6d78554661ac934
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:22:44 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,20 +7,31 @@ Usage: ./8-json_api.py <letter>
 The specified letter is sent as the value for the q variable.
 If no letter is provided, it sends q="" as the query parameter.
 """
-import requests
+# Import the necessary libraries
 import sys
+import requests
 
-
+# Check if the script is being run as the main program
 if __name__ == "__main__":
+    # Initialize an empty string 'letter'
     letter = "" if len(sys.argv) == 1 else sys.argv[1]
+
+    # Create a dictionary 'payload' with a single key "q" and the value of 'letter'
     payload = {"q": letter}
 
+    # Send an HTTP POST request to a local server at http://0.0.0.0:5000/search_user
     r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+
+    # Try to parse the response as JSON
     try:
         response = r.json()
+
+        # Check if the response is an empty dictionary
         if response == {}:
             print("No result")
         else:
+            # Print the "id" and "name" fields from the response in a formatted string
             print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
+        # Handle the case where the response is not valid JSON
         print("Not a valid JSON")
