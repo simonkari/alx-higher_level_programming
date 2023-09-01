e4c531a749799b1fe7fbcaada12186618269dd89
simonkari <kariithisimon1715@gmail.com>
2023-09-01 12:20:13 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-"""Module designed for retrieving data from 'https://intranet.hbtn.io/status' via HTTPS."""
-import urllib.request as request
-
+""" Module designed for retrieving data from 'https://alx-intranet.hbtn.io/status' via HTTPS. """
+import urllib.request
+import urllib.error
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as result:
-        body = result.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print('Body response:')
+            print(f'\t- type: {type(html)}')
+            print(f'\t- content: {html}')
+            print(f'\t- utf8 content: {html.decode("utf-8")}')
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
