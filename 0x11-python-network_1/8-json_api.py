12b73fecb36f9b97224bb91f7c9e0b19bfbd6ab0
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:21:14 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Dispatches a POST request to http://0.0.0.0:5000/search_user, specifying a particular letter.
+"""Dispatches a POST request to http://0.0.0.0:5000/search_user,
+specifying a particular letter.
 
 Usage: ./8-json_api.py <letter>
 
 The specified letter is sent as the value for the q variable.
 If no letter is provided, it sends q="" as the query parameter.
 """
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
