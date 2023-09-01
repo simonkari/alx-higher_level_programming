97cff221926c4eb2dd1af0101a674418b0ddc989
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:13:11 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""The script uses the GitHub API to display a GitHub ID based on credentials.
+"""Uses the GitHub API to display a GitHub ID based on given credentials.
 
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
 """
-import requests
 import sys
+import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
