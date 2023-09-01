4a8761a885627349ec649cfe55e7a51590fe372a
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:45:50 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,13 @@
 """ This is a Python script that retrieves information 
 from the URL "https://alx-intranet.hbtn.io/status
 """
-import urllib.request as request
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as result:
-        body = result.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
-- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
