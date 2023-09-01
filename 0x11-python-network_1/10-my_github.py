d8263282d63de1ad44180298f12d0a8c134686e0
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:13:39 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,17 +8,8 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 3:
-        print("Usage: ./10-my_github.py <GitHub username> <GitHub password>")
-        sys.exit(1)
-
-    # Create an authentication object using Basic Authentication
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-
-    # Send an HTTP GET request to the GitHub API for the authenticated user's information
     r = requests.get("https://api.github.com/user", auth=auth)
-
-    # Parse the JSON response and print the user's ID
     print(r.json().get("id"))
