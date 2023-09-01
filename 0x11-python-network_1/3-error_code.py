09b7605ecf678077a429679c2d5ef2177dede10f
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:26:54 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Initiates a request to a provided URL and showcases the content of the response body.
+"""Initiates a request to the provided URL and showcases
+the content of the response body.
 
 Usage: ./3-error_code.py <URL>
-  - Handles HTTP errors.
+
+Manages HTTP errors during the process.
 """
 
-import urllib.error
 import sys
+import urllib.error
 import urllib.request
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
+    # Check if the correct number of command-line arguments
     if len(sys.argv) != 2:
         print("Usage: ./3-error_code.py <URL>")
         sys.exit(1)
