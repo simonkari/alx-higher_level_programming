0948f025319a21354b6769bbed6efb28d82e1d6e
simonkari <kariithisimon1715@gmail.com>
2023-09-01 13:42:28 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-# URL to fetch
-url = "https://alx-intranet.hbtn.io/status"
 
-# Send an HTTP GET request to the URL and store the response in 'response'
-with urllib.request.urlopen(url) as response:
-    # Read the content of the response (the HTML body) and store it in the 'html' variable
-    html = response.read().decode('utf-8')
-
-    # Display the body of the response with proper tabulation
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
