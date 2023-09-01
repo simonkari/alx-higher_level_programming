ef06fd41a33dd23e7cc04b4eb7ce8389f3948d6f
simonkari <kariithisimon1715@gmail.com>
2023-09-01 12:10:43 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """ module that fetches https://intranet.hbtn.io/status"""
-import urllib.request
+import urllib.request as request
+
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    
-    with urllib.request.urlopen(url) as response:
-        html = response.read().decode("utf-8")
-    
-    print('Body response:')
-    print(f'\t- type: {type(html)}')
-    print(f'\t- content: {html}')
-    print(f'\t- utf8 content: {html}')
+    with request.urlopen("https://intranet.hbtn.io/status") as result:
+        body = result.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
+- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
