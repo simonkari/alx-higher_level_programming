4c4289003fd00530e6b1d23881cac91328a5e025
simonkari <kariithisimon1715@gmail.com>
2023-09-26 20:20:57 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
-const fs = require('fs').promises;
+// Shebang line specifying the interpreter
 
-async function main() {
-  try {
-    const data = await fs.readFile(process.argv[2], 'utf-8');
-    console.log(data);
-  } catch (err) {
-    console.error(err);
-  }
-}
+const fs = require('fs');
+// Import the 'fs' module to work with the file system
 
-if (process.argv[2]) {
-  main();
-} else {
-  console.error('Usage: node script.js <file-path>');
+if (process.argv.length > 2) {
+  // Check if command-line arguments are provided (excluding 'node' and the script filename)
+  
+  fs.readFile(process.argv[2], (err, data) => {
+    // Read the file specified by the first command-line argument (process.argv[2])
+    
+    if (err) {
+      // Handle any errors that may occur during file reading
+      console.log(err);
+    } else {
+      // If there are no errors, display the contents of the file as a UTF-8 string
+      console.log(data.toString('utf-8'));
+    }
+  });
 }
