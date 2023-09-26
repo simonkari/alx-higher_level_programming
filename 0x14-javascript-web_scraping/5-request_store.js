e9bbbdc707db8f46017209f79134b28bf1cf4da3
simonkari <kariithisimon1715@gmail.com>
2023-09-26 20:39:13 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
+
+// Import the 'fs' (file system) and 'request' modules
 const fs = require('fs');
 const request = require('request');
 
-if (process.argv.length === 4) {
-  const url = process.argv[2];
-  const filePath = process.argv[3];
-
-  // Make a GET request to the specified URL
-  request.get({ url, encoding: 'utf-8' }, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      // Write the response body to the specified file with UTF-8 encoding
-      fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-        if (err) {
-          console.error(`Error writing to file: ${err}`);
-        } else {
-          console.log(`Contents of ${url} saved to ${filePath}`);
-        }
-      });
-    } else {
-      console.error(`Error fetching URL: ${error || 'Status code ' + response.statusCode}`);
-    }
-  });
-} else {
-  console.error('Usage: node scriptName.js <URL> <FilePath>');
+// Check if there are at least 4 command line arguments
+if (process.argv.length > 3) {
+  // Use the 'request' module to make an HTTP GET request to
+  //the URL specified as the first command line argument (process.argv[2])
+  request
+    .get(`${process.argv[2]}`)
+    // Pipe the response of the HTTP request to create a writable 
+    //stream that writes the data to a file specified as the second 
+    //command line argument (process.argv[3])
+    .pipe(fs.createWriteStream(process.argv[3]));
 }
