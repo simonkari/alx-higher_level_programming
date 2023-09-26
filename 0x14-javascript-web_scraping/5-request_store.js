13ed1b8f516d9a8939d07c4ed1f6fc9ea2d57a84
simonkari <kariithisimon1715@gmail.com>
2023-09-26 20:42:31 +0300

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,10 @@ const request = require('request');
 // Check if there are at least 4 command line arguments
 if (process.argv.length > 3) {
   // Use the 'request' module to make an HTTP GET request to
-  //the URL specified as the first command line argument (process.argv[2])
+  //the URL specified as the first command line argument
   request
     .get(`${process.argv[2]}`)
-    // Pipe the response of the HTTP request to create a writable
-    //stream that writes the data to a file specified as the second
-    //command line argument (process.argv[3])
+    // Pipe the response of the HTTP request to create
+    //a writable stream
     .pipe(fs.createWriteStream(process.argv[3]));
 }
