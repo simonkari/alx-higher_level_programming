9fb206b2c7c50f00584fb1abb0a08a8a8d407bb0
simonkari <kariithisimon1715@gmail.com>
2023-09-26 20:22:23 +0300

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 
 if (process.argv.length > 2) {
   // Check if command-line arguments are provided (excluding 'node' and the script filename)
-  
+
   fs.readFile(process.argv[2], (err, data) => {
     // Read the file specified by the first command-line argument (process.argv[2])
-    
+
     if (err) {
       // Handle any errors that may occur during file reading
       console.log(err);
