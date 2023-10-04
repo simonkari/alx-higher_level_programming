f29ce3e286484689ffb63a9f89f618c66d68dfc3
simonkari <kariithisimon1715@gmail.com>
2023-10-04 20:21:27 +0300

    100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+'use strict';
+// This code uses the 'load' event listener to runs
+window.addEventListener('load', () => {
+  // Select the <header> element in the DOM using
+  //`document.querySelector`.
+  document.querySelector('header').style.color = '#FF0000';
+});
